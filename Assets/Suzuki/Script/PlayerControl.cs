72e3f7154b19d5fb44b6c6f45d4b99ae50c267e2
suzukiseiya1087 <138768571+suzukiseiya1087@users.noreply.github.com>
2024-02-29 11:39:31 +0900

    うさぎの本数を調整できるようにした

--- Assets/Suzuki/Script/PlayerControl.cs
+++ Assets/Suzuki/Script/PlayerControl.cs
@@ -20,7 +20,7 @@ public class PlayerControl : MonoBehaviour
     public float throwForce = 5f; // 木の実を投げる力
     public float pickupRange = 2f; // 木の実を拾う範囲
 
-    private int carrotCount = 5; // 最初は5本のにんじんを持っている
+   [SerializeField] private int carrotCount = 5; // 最初は5本のにんじんを持っている
 
     public GameObject carrotPrefab; // にんじんのプレファブ
 
